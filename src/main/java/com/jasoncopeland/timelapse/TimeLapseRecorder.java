e05aff7bf3fb41331d4c58ee68680901e7306b1c
Jason Copeland <copeland@adobe.com>
2016-03-13 13:41:03 -0700

    Adding Undertow web server (does nothing yet)

--- src/main/java/com/jasoncopeland/timelapse/TimeLapseRecorder.java
+++ src/main/java/com/jasoncopeland/timelapse/TimeLapseRecorder.java
@@ -2,6 +2,10 @@ package com.jasoncopeland.timelapse;
 
 import com.jasoncopeland.timelapse.imgsource.IImageSource;
 import com.jasoncopeland.timelapse.imgsource.URLImageSource;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 
 import java.awt.image.*;
 import java.util.ArrayList;
@@ -10,13 +14,24 @@ import java.util.List;
 public class TimeLapseRecorder {
 
     public static void main(String[] args) {
-        List<ImageTimelapse> timeLapses = new ArrayList<ImageTimelapse>();
+        final List<ImageTimelapse> timeLapses = new ArrayList<ImageTimelapse>();
         // new file on X frame, location, name
         timeLapses.add(new ImageTimelapse(450, 1440, args[0], "1440s"));
         timeLapses.add(new ImageTimelapse(300, 300, args[0], "300s"));
 //        timeLapses.add(new ImageMotion(300, 1, args[0], "motion"));
 
         IImageSource imageSource = new URLImageSource(args[1], args[2], args[3]);
+
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("test");
+                    }
+                }).build();
+        server.start();
 
         while (true) {
             try {
