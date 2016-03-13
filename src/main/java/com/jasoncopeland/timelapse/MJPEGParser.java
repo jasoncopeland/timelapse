bff280cd5a59355b7616e84e5ef24aa3bfe30bae
Jason Copeland <copeland@adobe.com>
2016-03-13 13:06:59 -0700

    Remove incorrect copyright header (none for now)

--- src/main/java/com/jasoncopeland/timelapse/MJPEGParser.java
+++ src/main/java/com/jasoncopeland/timelapse/MJPEGParser.java
@@ -1,20 +1,3 @@
-/*************************************************************************
- * ADOBE CONFIDENTIAL
- * ___________________
- * <p>
- * Copyright 2015-2016 Adobe Systems Incorporated
- * All Rights Reserved.
- * <p>
- * NOTICE:  All information contained herein is, and remains
- * the property of Adobe Systems Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Adobe Systems Incorporated and its
- * suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe Systems Incorporated.
- **************************************************************************/
 package com.jasoncopeland.timelapse;
 
 
--- src/main/java/com/jasoncopeland/timelapse/imgsource/URLImageSource.java
+++ src/main/java/com/jasoncopeland/timelapse/imgsource/URLImageSource.java
@@ -1,25 +1,7 @@
-/*************************************************************************
- * ADOBE CONFIDENTIAL
- * ___________________
- * <p>
- * Copyright 2015-2016 Adobe Systems Incorporated
- * All Rights Reserved.
- * <p>
- * NOTICE:  All information contained herein is, and remains
- * the property of Adobe Systems Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Adobe Systems Incorporated and its
- * suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe Systems Incorporated.
- **************************************************************************/
 package com.jasoncopeland.timelapse.imgsource;
 
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.net.URLConnection;
