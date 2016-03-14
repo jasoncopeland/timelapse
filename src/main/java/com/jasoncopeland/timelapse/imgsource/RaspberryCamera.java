d19a54f7fa0fda9c88eb7cd85c075ad4da5f7769
Jason Copeland <jason@jasoncopeland.com>
2016-03-13 23:37:05 -0700

    Raspberry pi updates - default image 800x600 - attempt to grab output & error and log

--- src/main/java/com/jasoncopeland/timelapse/imgsource/RaspberryCamera.java
+++ src/main/java/com/jasoncopeland/timelapse/imgsource/RaspberryCamera.java
@@ -2,7 +2,7 @@ package com.jasoncopeland.timelapse.imgsource;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by Jason on 3/13/2016.
@@ -10,10 +10,10 @@ import java.io.File;
 public class RaspberryCamera implements IImageSource {
 
     protected final String raspistillPath = "/opt/vc/bin/raspistill";
-    protected final int captureTimeoutInMS = 5000;
+    protected final int captureTimeoutInMS = 10000;
     protected final int imgQuality = 100;
-    protected int imgWidth = 0;
-    protected int imgHeight = 0;
+    protected int imgWidth = 800;
+    protected int imgHeight = 600;
     protected String imageType = "png";
 
     public BufferedImage getCurrentImage() {
@@ -37,6 +37,11 @@ public class RaspberryCamera implements IImageSource {
 
             Process process = Runtime.getRuntime().exec(sb.toString());
             if (process != null && process.waitFor() == 0) {
+                String error = readInputStreamAsString(process.getErrorStream());
+                String output = readInputStreamAsString(process.getErrorStream());
+                if (error != null || output != null) {
+                    System.out.println("capture cmd output: \"" + output + "\" error: \"" + error + "\"");
+                }
                 return ImageIO.read(new File(fileName));
             }
 
@@ -46,5 +51,19 @@ public class RaspberryCamera implements IImageSource {
             System.out.println("Total pi camera capture time: " + (System.currentTimeMillis() - startTime) + "ms");
         }
         return null;
+    }
+
+    public static String readInputStreamAsString(InputStream in)
+            throws IOException {
+
+        BufferedInputStream bis = new BufferedInputStream(in);
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        int result = bis.read();
+        while(result != -1) {
+            byte b = (byte)result;
+            buf.write(b);
+            result = bis.read();
+        }
+        return buf.toString();
     }
 }
